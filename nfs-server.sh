6c3e0d9a3ee2d2e7686277f1f7e056a067b1f3b5
ktjkmu98 <92903344+ktjkmu98@users.noreply.github.com>
2024-04-02 17:39:25 +0900

    Update nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /nfs-shared
 chmod 777 nfs-shared
-ech "/nfs-shared 10.0.0.0/14(rw, sync)" >> /etc/exports
+ech "/nfs-shared 10.0.0.0/14(rw,sync)" >> /etc/exports
 systemctl restart nfs-server
 systemctl enable nfs-server
