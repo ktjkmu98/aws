dab2e1997785cf047a2b4a5ab514cc7e4da85a2d
ktjkmu98 <92903344+ktjkmu98@users.noreply.github.com>
2024-04-02 15:52:58 +0900

    Create nat-instance.sh

---
+++ nat-instance.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+yum install -y iptables-services
+systemctl start iptables.services
+systemctl enable iptables.services
+iptables -F
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
