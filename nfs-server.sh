b04a9a1aab9b8fa5fce831402f40a371a6666e29
ktjkmu98 <92903344+ktjkmu98@users.noreply.github.com>
2024-04-03 18:04:35 +0900

    Update nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /nfs-shared
-chmod 777 nfs-shared
-ech "/nfs-shared 10.0.0.0/14(rw,sync)" >> /etc/exports
+chmod 777 /nfs-shared
+echo "/nfs-shared 10.0.0.0/14(rw,sync)" >> /etc/exports
 systemctl restart nfs-server
 systemctl enable nfs-server
 ec2-metadata -o | cut -d ' ' -f 2 >> /nfs-shared/list.txt
